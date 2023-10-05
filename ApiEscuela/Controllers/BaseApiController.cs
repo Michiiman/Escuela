d51bfe162ee81e807d97fef6a3882fc991e78122
DevJLeon <celisjoshleon@gmail.com>
2023-10-05 15:06:17 -0500

    feat: :sparkles: BaseApiController

---
+++ ApiEscuela/Controllers/BaseApiController.cs
@@ -0,0 +1,13 @@
+
+
+using Microsoft.AspNetCore.Mvc;
+
+namespace ApiEscuela.Controllers;
+
+[ApiController]
+[Route("api/escuela/[controller]")]
+
+public class BaseApiController:ControllerBase
+{
+    
+}
