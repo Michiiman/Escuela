b39ca5683ffaab3a9daa75de2180e8122363859a
DevJLeon <celisjoshleon@gmail.com>
2023-10-06 15:04:04 -0500

    Interfaces & Repos skeleton

--- Application/Repository/StudentRepository.cs
+++ Application/Repository/StudentRepository.cs
@@ -2,11 +2,12 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Domain.Entities;
+using Domain.Interfaces;
+using Persistence;
 
-namespace Application.Repository
+namespace Application.Repository;
+public class StudentRepository:GenericRepository<Student>, IStudentRepository
 {
-    public class StudentRepository
-    {
-        
-    }
+    public StudentRepository(ApiEscuelaContext context) : base(context){}
 }
